9283426c9bbab98a3f20d2e4017a0f0517b1e0ad
arara90 <ararajo@gmail.com>
2024-06-24 12:36:27 +0900

    feat: updateTel  추가

--- src/parent-user/parent-user.controller.ts
+++ src/parent-user/parent-user.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Put } from '@nestjs/common'
+import { ParentUserService } from './parent-user.service'
+import { UpdatePhone } from '../../subtree-types/user-service/interface/update-phone.interface'
 
 @Controller('parent-user')
-export class ParentUserController {}
+export class ParentUserController {
+  constructor(private readonly parentUserService: ParentUserService) {}
+
+  @Put()
+  async updatePhone(
+    @Body() body: UpdatePhone,
+  ): Promise<string> {
+    return this.parentUserService.updatePhone(body)
+  }
+}
--- src/parent-user/parent-user.service.ts
+++ src/parent-user/parent-user.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { UpdatePhone } from '../../subtree-types/user-service/interface/update-phone.interface'
 
 @Injectable()
-export class ParentUserService {}
+export class ParentUserService {
+
+  async updatePhone(opt: UpdatePhone) {
+    console.log(opt)
+    return 'done'
+  }
+}
